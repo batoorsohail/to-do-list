6ee204b2f9003129f74b7c962bb1d94f32ed91bf
unknown <sohailshahidi1435@gmail.com>
2023-02-10 10:00:24 +0430

    The branch should be deleted

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
-  devServer: {
-    static: './dist',
-  },
   output: {
-    filename: 'static/js/[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    runtimeChunk: 'single',
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/"
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
-  mode: 'development',
+
+  optimization: {
+    runtimeChunk: "single",
+  },
 };
